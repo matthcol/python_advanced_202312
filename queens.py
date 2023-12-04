9b33f772336fc9f7f0ee25576d01fd81bced196a
matthcol <matthias.colin@gmail.com>
2023-12-04 15:02:28 +0100

    iterator, generator

---
+++ queens.py
@@ -0,0 +1,21 @@
+# code from the community (unknown source)
+
+def queens(n):
+    a = list(range(n))
+    up = [True] * (2 * n - 1)
+    down = [True] * (2 * n - 1)
+    def sub(i):
+        for k in range(i, n):
+            j = a[k]
+            p = i + j
+            q = i - j + n - 1
+            if up[p] and down[q]:
+                if i == n - 1:
+                    yield tuple(a)
+                else:
+                    up[p] = down[q] = False
+                    a[i], a[k] = a[k], a[i]
+                    yield from sub(i + 1)
+                    up[p] = down[q] = True
+                    a[i], a[k] = a[k], a[i]
+    yield from sub(0)
